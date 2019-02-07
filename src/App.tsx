fcf6659ee240310a13a85e6015d31d7932b321bc
evblance <evblance@users.noreply.github.com>
2019-02-07 12:21:56 +1100

    Adds a styled AppTitle to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
         <React.Fragment>
-            
+            <AppTitle>Dungeon Crawler</AppTitle>
         </React.Fragment>
     );
   }
 }
 
 export default App;
+
+const AppTitle = styled.h1`
+    color: var(--tertiaryColour);
+    font: 3rem var(--headingFont);
+    text-align: center;
+`
