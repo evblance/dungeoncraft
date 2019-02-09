6b39376c052d70d259cc336af2ae4e516d9d4a8f
evblance <evblance@users.noreply.github.com>
2019-02-10 10:38:27 +1100

    Updates Collider class to have a scale property in addition to its offset

--- src/classes/collider.class.ts
+++ src/classes/collider.class.ts
@@ -1,19 +1,20 @@
 import GameObject from './game-object.class';
 import Position from './position.class';
+import Scale from './scale.class';
+import IVector2D from '../interfaces/vector-2d.interface';
 import { IBoundingRect } from '../interfaces/bounding-rect.interface';
 import { CONSTANTS } from '../data/constants.data';
 
 export default class Collider extends GameObject {
 
-    dx: number;
-    dy: number;
+    scale: Scale;
+    offset: IVector2D;
 
     public getBounds(position: Position): IBoundingRect {
-        const top = position.y - this.dy;
-        const right =  position.x + this.dx;
-        const bottom = position.y + this.dy;
-        const left = position.x - this.dx;
-
+        const top = position.y + this.offset.Y;
+        const right =  position.x + this.scale.x + this.offset.X;
+        const bottom = position.y + this.scale.y + this.offset.Y;
+        const left = position.x + this.offset.X;
         return {
             TOP: top,
             RIGHT: right,
@@ -23,12 +24,12 @@ export default class Collider extends GameObject {
     }
 
     constructor(
-        dx: number = CONSTANTS.ENTITY_DEFAULT_SCALE / 2,
-        dy: number = CONSTANTS.ENTITY_DEFAULT_SCALE / 2,
+        scale: Scale = new Scale({X: CONSTANTS.ENTITY_DEFAULT_SCALE, Y: CONSTANTS.ENTITY_DEFAULT_SCALE}),
+        offset: IVector2D = {X: 0, Y: 0},
     ) {
         super();
-        this.dx = dx;
-        this.dy = dy;
+        this.scale = scale;
+        this.offset = offset;
     }
 
 }
